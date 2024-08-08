2f25c4de213ae0a2b6f4451f7b7901d3f9b92c8a
b1tflyyyy <thisisandrew0110@gmail.com>
2024-08-08 04:22:42 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-#include <bfl-perf-timer.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 
@@ -26,14 +25,7 @@ int main(int argc, char** argv)
     ascii_converter.Set_Base_Image(&image);
     ascii_converter.Resize_Image(cv::Size{ 400, 150 });
     
-    { // perf test 
-        bfl::auto_perf_timer<> timer{ std::cout };
-        for (std::size_t i{}; i < 500; ++i)
-        {
-            ascii_converter.Convert_Image_To_Ascii();
-        }
-    }
-
+    ascii_converter.Convert_Image_To_Ascii();
     ascii_converter.Save_Image_To_File("ascii_image.txt");
     
     const auto& formatted_image{ ascii_converter.Get_Formatted_Output_Ascii_Image() };
