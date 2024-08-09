c78f35e82b68836061af40d03b7373001bfabec2
b1tflyyyy <thisisandrew0110@gmail.com>
2024-08-09 06:54:24 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 
-#define ENABLE_ONLY_IMAGES false
+#define ENABLE_ONLY_IMAGES true
 
 int main(int argc, char** argv)
 {
@@ -56,7 +56,7 @@ int main(int argc, char** argv)
         cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
 
         ascii_converter.Set_Base_Image(&frame);
-        ascii_converter.Resize_Image(cv::Size{ 500, 155 });
+        ascii_converter.Resize_Image(cv::Size{ 300, 100 });
         ascii_converter.Convert_Image_To_Ascii();
         const auto& formatted_image{ ascii_converter.Get_Formatted_Output_Ascii_Image(AAscii_Converter::EOutput_Formatted_Ascii_Image::CALCULATE) };
         std::cout << std::data(formatted_image);
