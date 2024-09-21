c84a0f810c0a9b494615183655401232edf64d42
b1tfly <thisisandrew0110@gmail.com>
2024-09-20 12:03:50 -1200

    added positive/negative conversion

--- ascii-converter/ascii-converter.cpp
+++ ascii-converter/ascii-converter.cpp
@@ -1,13 +1,14 @@
 #include "ascii-converter.hpp"
 
 AAscii_Converter::AAscii_Converter() :
-    Base_Image{ nullptr }, 
+    Base_Image{ nullptr },
     Ascii_Image{},
-    Look_Up_Table(1, 256, CV_8U, cv::Scalar::all(0)),
+    Look_Up_Table_Base(1, 256, CV_8U, cv::Scalar::all(0)),
+    Look_Up_Table_Negative(1, 256, CV_8U, cv::Scalar::all(0)),
     Formatted_Output_Ascii_Image{},
     Ascii_Art_Symbols{ '.', '-', '=', '*', ':', 'o', '&', '8', '#', '@' }
-{  
-    Create_Look_Up_Table();
+{
+    Create_Look_Up_Tables();
 }
 
 void AAscii_Converter::Set_Base_Image(cv::Mat* base_image)
@@ -28,25 +29,33 @@ void AAscii_Converter::Set_Base_Image(cv::Mat* base_image)
 
 void AAscii_Converter::Resize_Image(const cv::Size size)
 {
-    cv::Mat tmp{}; 
+    cv::Mat tmp{};
     cv::resize(*Base_Image, tmp, size);
 
     *Base_Image = tmp;
 }
 
-void AAscii_Converter::Convert_Image_To_Ascii()
+void AAscii_Converter::Convert_Image_To_Ascii(const bool is_negative)
 {
-    cv::LUT(*Base_Image, Look_Up_Table, Ascii_Image);
+    if (!is_negative)
+    {
+        cv::LUT(*Base_Image, Look_Up_Table_Base, Ascii_Image);
+        return;
+    }
+
+    cv::LUT(*Base_Image, Look_Up_Table_Negative, Ascii_Image);
 }
 
 void AAscii_Converter::Save_Image_To_File(const std::filesystem::path& path)
 {
+    using namespace std::string_literals;
+
     std::ofstream fout{ path.native(), std::ios::out | std::ios::trunc };
     if (!fout.is_open())
     {
-        throw std::runtime_error{ std::format("can not open file: {}", path.native()) };
+        throw std::runtime_error{ "can not open file: "s + path.string() };
     }
- 
+
     const uchar* ascii_ptr{ Ascii_Image.ptr() };
     for (std::size_t i{}; i < Base_Image->rows; ++i)
     {
@@ -78,9 +87,20 @@ std::size_t AAscii_Converter::Map_Values(const float old_value, const float old_
     return static_cast<std::size_t>(((old_value - old_value_min) / (old_value_max - old_value_min)) * (new_max - new_min) + new_min);
 }
 
-void AAscii_Converter::Create_Look_Up_Table()
+void AAscii_Converter::Create_Look_Up_Tables()
 {
-    uchar* ptr{ Look_Up_Table.ptr() };
+    // create base look up table
+    uchar* ptr{ Look_Up_Table_Base.ptr() };
+    for (std::size_t i{}; i < 256; ++i)
+    {
+        const auto mapped_index{ Map_Values(i, 0, 255, 0, Ascii_Art_Symbols.size() - 1) };
+        ptr[i] = Ascii_Art_Symbols[mapped_index];
+    }
+
+    // create negative look up table
+    std::ranges::reverse(std::ranges::begin(Ascii_Art_Symbols), std::ranges::end(Ascii_Art_Symbols));
+
+    ptr = Look_Up_Table_Negative.ptr();
     for (std::size_t i{}; i < 256; ++i)
     {
         const auto mapped_index{ Map_Values(i, 0, 255, 0, Ascii_Art_Symbols.size() - 1) };
@@ -91,22 +111,21 @@ void AAscii_Converter::Create_Look_Up_Table()
 void AAscii_Converter::Calculate_Formatted_Output_Ascii_Image()
 {
     Formatted_Output_Ascii_Image.clear();
-    
+
     // +1 for '\n' and for '\0'
     Formatted_Output_Ascii_Image.resize(static_cast<std::size_t>(Ascii_Image.cols + 1) * static_cast<std::size_t>(Ascii_Image.rows) + 1);
-    
+
     uchar* formatted_ascii_ptr{ Formatted_Output_Ascii_Image.data() };
     for (std::ptrdiff_t i{}; i < Ascii_Image.rows; ++i)
     {
         const auto ascii_image_offset{ i * static_cast<decltype(i)>(Ascii_Image.cols) };
 
-        std::ranges::copy_n(Ascii_Image.ptr() + ascii_image_offset, 
-                            Ascii_Image.cols, 
-                            formatted_ascii_ptr);
-               
+        std::ranges::copy_n(Ascii_Image.ptr() + ascii_image_offset,
+            Ascii_Image.cols,
+            formatted_ascii_ptr);
+
         formatted_ascii_ptr += Ascii_Image.cols;
         *formatted_ascii_ptr = '\n';
-        ++formatted_ascii_ptr;   
+        ++formatted_ascii_ptr;
     }
 }
-
--- ascii-converter/ascii-converter.hpp
+++ ascii-converter/ascii-converter.hpp
@@ -7,9 +7,9 @@
 #include <stdexcept>
 #include <fstream>
 #include <algorithm>
+#include <format>
+#include <array>
 
-#include <opencv2/core/hal/interface.h>
-#include <opencv2/core/types.hpp>
 #include <opencv2/opencv.hpp>
 
 class AAscii_Converter
@@ -24,25 +24,26 @@ public:
 public:
     explicit AAscii_Converter();
     ~AAscii_Converter() = default;
-    
-    void Set_Base_Image(cv::Mat* base_image); 
-    
+
+    void Set_Base_Image(cv::Mat* base_image);
+
     void Resize_Image(const cv::Size size);
-    void Convert_Image_To_Ascii();
+    void Convert_Image_To_Ascii(const bool is_negative = false);
     void Save_Image_To_File(const std::filesystem::path& path);
-    
+
     const cv::Mat& Get_Ascii_Image() const noexcept;
     const std::vector<uchar>& Get_Formatted_Output_Ascii_Image(EOutput_Formatted_Ascii_Image action = EOutput_Formatted_Ascii_Image::CALCULATE);
 
 private:
     inline std::size_t Map_Values(const float old_value, const float old_value_min, const float old_value_max, const float new_min, const float new_max);
-    void Create_Look_Up_Table();
+    void Create_Look_Up_Tables();
     void Calculate_Formatted_Output_Ascii_Image();
 
 private:
     cv::Mat* Base_Image;
     cv::Mat Ascii_Image;
-    cv::Mat Look_Up_Table;
+    cv::Mat Look_Up_Table_Base;
+    cv::Mat Look_Up_Table_Negative;
     std::vector<uchar> Formatted_Output_Ascii_Image;
     std::array<uchar, 10> Ascii_Art_Symbols;
 };
